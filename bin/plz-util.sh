98e1549eff40c1fae0aacfa8aa5bb7d2be885523
Daniel Brockman <daniel@brockman.se>
2013-07-15 10:43:38 +0200

    Rewrite, simplify and rename to exec-longest-prefix

--- bin/plz-util.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-function die { echo >&2 $*; exit 1; }
-function error { die plz: error: $*; }
-function trace { [[ -z $PLZ_TRACE ]] || echo plz: trace: $* >&2; }
-function trace-do { trace $(shell-quote "$@"); "$@"; }
-function shell-quote { git rev-parse --sq-quote "${@}"; }
